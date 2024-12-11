34d3349119bca473c3499b33c59d61c0bb9bd29e
SpriiteL <nathanpa.etude@gmail.com>
2024-12-11 10:57:02 +0100

    gestion des droits v1

---
+++ src/Security/AccessDeniedHandler.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+
+class AccessDeniedHandler implements AccessDeniedHandlerInterface
+{
+    public function handle(Request $request, \Throwable $accessDeniedException): ?RedirectResponse
+    {
+        // Redirection vers la route "app_inventory"
+        return new RedirectResponse('/inventory');
+    }
+}

17d95d89e42995885679c9c844acd7b017437301
Mathieu-Lije <160591675+Mathieu-Lije@users.noreply.github.com>
2025-01-31 19:12:43 +0100

    Integrate Vue.js with Symfony and configure Webpack Encore

--- assets/app.js
+++ assets/app.js
@@ -1,11 +1,13 @@
 import './bootstrap.js';
+
 /*
  * Welcome to your app's main JavaScript file!
  *
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-
+// Importer vue js
+import './js/app.js';
 // Importer les styles Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 

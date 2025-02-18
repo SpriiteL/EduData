280893b79ec3f9abc266b831407959ac6e85a6a5
SpriiteL <nathanpa.etude@gmail.com>
2025-02-18 08:56:35 +0100

    ajout entity imprimante

---
+++ migrations/Version20250218075505.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250218075505 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE imprimante (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(255) NOT NULL, printer VARCHAR(255) NOT NULL, nb_copy_bw INT NOT NULL, nb_copy_color INT NOT NULL, id_printer VARCHAR(255) NOT NULL, name_printer VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE imprimante');
+    }
+}
---
+++ src/Entity/Imprimante.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ImprimanteRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ImprimanteRepository::class)]
+class Imprimante
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $username = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $printer = null;
+
+    #[ORM\Column]
+    private ?int $nbCopyBW = null;
+
+    #[ORM\Column]
+    private ?int $nbCopyColor = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $idPrinter = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $namePrinter = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): static
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getPrinter(): ?string
+    {
+        return $this->printer;
+    }
+
+    public function setPrinter(string $printer): static
+    {
+        $this->printer = $printer;
+
+        return $this;
+    }
+
+    public function getNbCopyBW(): ?int
+    {
+        return $this->nbCopyBW;
+    }
+
+    public function setNbCopyBW(int $nbCopyBW): static
+    {
+        $this->nbCopyBW = $nbCopyBW;
+
+        return $this;
+    }
+
+    public function getNbCopyColor(): ?int
+    {
+        return $this->nbCopyColor;
+    }
+
+    public function setNbCopyColor(int $nbCopyColor): static
+    {
+        $this->nbCopyColor = $nbCopyColor;
+
+        return $this;
+    }
+
+    public function getIdPrinter(): ?string
+    {
+        return $this->idPrinter;
+    }
+
+    public function setIdPrinter(string $idPrinter): static
+    {
+        $this->idPrinter = $idPrinter;
+
+        return $this;
+    }
+
+    public function getNamePrinter(): ?string
+    {
+        return $this->namePrinter;
+    }
+
+    public function setNamePrinter(string $namePrinter): static
+    {
+        $this->namePrinter = $namePrinter;
+
+        return $this;
+    }
+}

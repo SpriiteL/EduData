bf58459dd4a66cfd11a5051cbb1477ef9f615889
SpriiteL <nathanpa.etude@gmail.com>
2025-04-24 13:40:18 +0200

    fix badges

--- src/Form/BadgeType.php
+++ src/Form/BadgeType.php
@@ -1,31 +1,31 @@
 <?php
-
-namespace App\Form;
-
-use App\Entity\Badge;
-use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
-
-class BadgeType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
-        $builder
-            ->add('Nom')
-            ->add('Prenom')
-            ->add('Date', null, [
-                'widget' => 'single_text',
-            ])
-            ->add('Classe')
-            ->add('etatTraitement')
-        ;
-    }
-
-    public function configureOptions(OptionsResolver $resolver): void
-    {
-        $resolver->setDefaults([
-            'data_class' => Badge::class,
-        ]);
-    }
-}
+ 
+ namespace App\Form;
+ 
+ use App\Entity\Badge;
+ use Symfony\Component\Form\AbstractType;
+ use Symfony\Component\Form\FormBuilderInterface;
+ use Symfony\Component\OptionsResolver\OptionsResolver;
+ 
+ class BadgeType extends AbstractType
+ {
+     public function buildForm(FormBuilderInterface $builder, array $options): void
+     {
+         $builder
+             ->add('Nom')
+             ->add('Prenom')
+             ->add('Date', null, [
+                 'widget' => 'single_text',
+             ])
+             ->add('Classe')
+             ->add('etatTraitement')
+         ;
+     }
+ 
+     public function configureOptions(OptionsResolver $resolver): void
+     {
+         $resolver->setDefaults([
+             'data_class' => Badge::class,
+         ]);
+     }
+ }

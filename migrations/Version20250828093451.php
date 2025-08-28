800e82a26dfd7d7c7a3ffe3b98d1ef5b25f45d4c
SpriiteL <nathanpa.etude@gmail.com>
2025-08-28 11:35:39 +0200

    fix lieu badge

---
+++ migrations/Version20250828093451.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250828093451 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE badge CHANGE date date DATE NOT NULL, CHANGE classe classe VARCHAR(100) NOT NULL, CHANGE etat_traitement etat_traitement VARCHAR(50) NOT NULL
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE badge CHANGE date date DATETIME NOT NULL, CHANGE classe classe VARCHAR(255) NOT NULL, CHANGE etat_traitement etat_traitement VARCHAR(255) NOT NULL
+        SQL);
+    }
+}
